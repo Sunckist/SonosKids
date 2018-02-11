784606fd2b48ff7d3e27bff152ab5710e5f74ef5
Magnus Lejonlid <magnus.lejonlid@gmail.com>
2018-02-11 16:39:25 +0100

    - Toggle play now plays the next item in the queue, or the first if at end of queue - Added colored frame to album artwork

--- app/src/main/java/com/backstream/sonoskids/TogglePlayTask.java
+++ app/src/main/java/com/backstream/sonoskids/TogglePlayTask.java
@@ -6,8 +6,10 @@ import com.vmichalak.sonoscontroller.SonosDevice;
 import com.vmichalak.sonoscontroller.exception.SonosControllerException;
 import com.vmichalak.sonoscontroller.model.PlayState;
 import com.vmichalak.sonoscontroller.model.TrackInfo;
+import com.vmichalak.sonoscontroller.model.TrackMetadata;
 
 import java.io.IOException;
+import java.util.List;
 
 public class TogglePlayTask extends AsyncTask<SonosDevice,Void,TrackInfo> {
     private final SonosCallback callback;
@@ -28,7 +30,14 @@ public class TogglePlayTask extends AsyncTask<SonosDevice,Void,TrackInfo> {
                 if (sonosDevice.getPlayState().equals(PlayState.PLAYING)) {
                     sonosDevice.pause();
                 } else {
-                    sonosDevice.play();
+                    List<TrackMetadata> queue = sonosDevice.getQueue(0, Integer.MAX_VALUE);
+                    int queueIndex = sonosDevice.getCurrentTrackInfo().getQueueIndex();
+                    if (queueIndex >= queue.size()) {
+                        sonosDevice.playFromQueue(1);
+                    } else {
+                        sonosDevice.next();
+                        sonosDevice.play();
+                    }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
